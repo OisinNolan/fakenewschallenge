7e7f8a4b494ecf97e45bfdfc027cfa7126491292
callumtilbury <callum@tilbury.co.za>
2022-03-11 22:13:46 +0000

    Add loop through epochs of train/valid.

--- nli_classifier/train.py
+++ nli_classifier/train.py
@@ -9,9 +9,9 @@ from torch import nn
 from tqdm import tqdm
 import math
 
-
-BATCH_SIZE = 10
-LEARNING_RATE = 0.05
+EPOCHS = 10
+BATCH_SIZE = 64
+LEARNING_RATE = 0.1
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 def train_loop(dataloader, model, loss_fn, optimizer):
@@ -47,16 +47,14 @@ def test_loop(dataloader, model, loss_fn):
     }
 
     with torch.no_grad():
-        for (H, B), y in dataloader:
+        for (H, B), y in tqdm(dataloader):
+            y = y.to(DEVICE)
             B_pad = pad_tokenize(B)
             pred = model(H, B_pad)
             test_loss += loss_fn(pred, y).item()
             for pred_i, y_i in zip(pred, y):
                 correct[int(y_i)] += int(pred_i.argmax() == y_i)
                 class_size[int(y_i)] += 1
-            print("Per-class avg:")
-            print([f'Class {i}: {correct[i] / class_size[i]}' for i in class_size.keys()])
-            print(f"Avg loss: {test_loss:>8f}\n")
 
     test_loss /= num_batches
     print("Per-class avg:")
@@ -64,9 +62,10 @@ def test_loop(dataloader, model, loss_fn):
     print(f"Avg loss: {test_loss:>8f}\n")
 
 dataset = FakeNewsDataset('../data/combined_stances_train.csv', '../data/combined_bodies_train.csv', related_only=True)
+SUBSET_SIZE = 128 # len(dataset)
 
 # Partition dataset into train and val sets
-dataset_size = len(dataset)
+dataset_size = SUBSET_SIZE
 dataset_indices = list(range(dataset_size))
 cutoff = math.floor(dataset_size * 0.7)
 train_sampler = SubsetRandomSampler(dataset_indices[:cutoff])
@@ -81,4 +80,11 @@ model.to(DEVICE)
 loss_fn = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=LEARNING_RATE)
 
-test_loop(val_dataloader, model, loss_fn)
+for epoch in range(EPOCHS):
+    # Train
+    model.train()
+    train_loop(train_dataloader, model, loss_fn, optimizer)
+
+    # Eval
+    model.eval()
+    test_loop(val_dataloader, model, loss_fn)
