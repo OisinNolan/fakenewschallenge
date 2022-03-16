3976c5a5284b46ebb003ff92cab0467619123956
Oisín <oinolan@tcd.ie>
2022-03-16 16:17:54 +0000

    Added simple mlp model

--- nli_classifier/model.py
+++ nli_classifier/model.py
@@ -10,15 +10,17 @@ NLI_DIM = 768
 NUM_CLASSES = 3
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 SIM_SCALAR = 50
+HDIM = 100
 
 class AgreemNet(nn.Module):
     def __init__(self):
         super(AgreemNet, self).__init__()
-        self.sim_encoder = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+        # self.sim_encoder = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
         self.nli_encoder = SentenceTransformer('sentence-transformers/nli-distilroberta-base-v2')
-        self.attention = torch.nn.MultiheadAttention(embed_dim=SIM_DIM, vdim=NLI_DIM, num_heads=1)
-        self.reduce_head = torch.nn.Linear(NLI_DIM, SIM_DIM)
-        self.classifier_fully_connected = torch.nn.Linear(1 + (SIM_DIM * 2), NUM_CLASSES)
+        # self.attention = torch.nn.MultiheadAttention(embed_dim=SIM_DIM, vdim=NLI_DIM, num_heads=1)
+        # self.reduce_head = torch.nn.Linear(NLI_DIM, SIM_DIM)
+        self.hidden = torch.nn.Linear((NLI_DIM * 6) + 5, HDIM)
+        self.classifier_fully_connected = torch.nn.Linear(HDIM, NUM_CLASSES)
 
     def encode_(self, encoder, embed_dim, B_flat):
         '''
@@ -39,25 +41,30 @@ class AgreemNet(nn.Module):
         B: List of padded lists of body sentence strings
         '''
         batch_size = len(B)
-        sent_len = len(B[0])
+        # Only include first 5 sentences
+        sent_len = 5
         # flatten list of lists of lists -> list of lists
-        B_flat = reduce(lambda x, y: x+y, B)
+        B_flat = np.array(B)[:,:5].flatten()
 
         # Compute embeddings
-        head_sim = torch.from_numpy(self.sim_encoder.encode(H)).to(DEVICE) * SIM_SCALAR
-        body_sims = self.encode_(self.sim_encoder, SIM_DIM, B_flat).view(batch_size, sent_len, SIM_DIM).to(DEVICE) * SIM_SCALAR
+        # head_sim = torch.from_numpy(self.sim_encoder.encode(H)).to(DEVICE) * SIM_SCALAR
+        # body_sims = self.encode_(self.sim_encoder, SIM_DIM, B_flat).view(batch_size, sent_len, SIM_DIM).to(DEVICE) * SIM_SCALAR
         head_nli = torch.from_numpy(self.nli_encoder.encode(H)).to(DEVICE)
         body_nlis = self.encode_(self.nli_encoder, NLI_DIM, B_flat).view(batch_size, sent_len, NLI_DIM).to(DEVICE)
 
         # Attention layer
-        attn_out, attn_weights = self.attention(
-            head_sim.view(1, batch_size, SIM_DIM),
-            body_sims.view(sent_len, batch_size, SIM_DIM),
-            body_nlis.view(sent_len, batch_size, NLI_DIM)
-        )
+        # attn_out, attn_weights = self.attention(
+        #     head_sim.view(1, batch_size, SIM_DIM),
+        #     body_sims.view(sent_len, batch_size, SIM_DIM),
+        #     body_nlis.view(sent_len, batch_size, NLI_DIM)
+        # )
+
+        cosines = torch.zeros((batch_size, sent_len))
+        for i in range(batch_size):
+            cosines[i] = F.cosine_similarity(head_nli[i].unsqueeze(0).repeat(5, 1), body_nlis[i])
 
         # Linear transform head_nli to be same dimension as attn_out
-        reduced_head = self.reduce_head(head_nli)
-        catted = torch.cat((attn_out.squeeze(), reduced_head, F.cosine_similarity(attn_out.squeeze(), reduced_head).view(batch_size, 1)), dim=1)
-        logits = self.classifier_fully_connected(catted)
+        catted = torch.cat((head_nli, body_nlis.flatten(1), cosines), dim=1)
+        hidden_layer = self.hidden(catted)
+        logits = self.classifier_fully_connected(hidden_layer)
         return F.softmax(logits, dim=1)
