4420fa701f10fe72ff837a09e178a73d44ac9a80
callumtilbury <callum@tilbury.co.za>
2022-03-22 11:41:12 +0000

    New train loop for AgreemFlat model.

--- train.py
+++ train.py
@@ -1,8 +1,7 @@
-from dataset import FakeNewsDataset
+from dataset import FakeNewsEncodedDataset
 from torch.utils.data import DataLoader
 from torch.utils.data.sampler import SubsetRandomSampler
-from dataset import FakeNewsDataset
-from model import AgreemNet
+from model import AgreemFlat, AgreemNet
 from util import pad_tokenize 
 import torch
 from torch import batch_norm, nn
@@ -10,13 +9,14 @@ from tqdm import tqdm
 import math
 import wandb
 
-# wandb.init(project="first-tests", entity="mlpbros")
-
 EPOCHS = 10
-BATCH_SIZE = 2
+BATCH_SIZE = 64
 LEARNING_RATE = 0.001
 EVAL_FREQ = 50
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+VAL_CUTOFF = 0.7
+
+wandb.init(project="yo-am-i-going-crazy", entity="mlpbros")
 
 wandb.config = {
    "learning_rate": LEARNING_RATE,
@@ -24,30 +24,26 @@ wandb.config = {
    "batch_size": BATCH_SIZE,
 }
 
-def train_loop(dataloader, model, loss_fn, optimizer):
-    size = len(dataloader.dataset)
-    for batch, ((H, B), y) in tqdm(enumerate(dataloader)):
+def train(dataloader, model, loss_fn, optimizer):
+    for batch, (embeddings, stance) in tqdm(enumerate(dataloader)):
         model.train()
-        B_pad = pad_tokenize(B)
-        # Compute prediction and loss
-        pred = model(H, B_pad).to(DEVICE)
-        loss = loss_fn(pred, y.to(DEVICE))
+        pred = model(*embeddings)
+        loss = loss_fn(pred, stance) # TODO: use weight for unbalanced??
 
         # Backpropagation
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
-        # if batch % 1 == 0:
         loss, current = loss.item(), (batch * BATCH_SIZE)
-        print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+        tqdm.write(f"{current:>5d} | loss: {loss:>7f}")
         wandb.log({"training-loss": loss})
 
-        if (batch % EVAL_FREQ == 0 and False):
+        if (batch % EVAL_FREQ == 0):
             model.eval()
-            val_loop(val_dataloader, model, loss_fn)
+            val(val_dataloader, model, loss_fn)
 
-def val_loop(dataloader, model, loss_fn):
+def val(dataloader, model, loss_fn):
     print("*"*20,"VALIDATION","*"*20)
     size = len(dataloader.dataset)
     num_batches = len(dataloader)
@@ -55,60 +51,59 @@ def val_loop(dataloader, model, loss_fn):
     correct = {
         0: 0,
         1: 0,
-        2: 0
+        2: 0,
+        3: 0,
     }
     class_size = {
         0: 0,
         1: 0,
-        2: 0
+        2: 0,
+        3: 0,
     }
 
     with torch.no_grad():
-        for (H, B), y in tqdm(dataloader):
-            y = y.to(DEVICE)
-            B_pad = pad_tokenize(B)
-            pred = model(H, B_pad)
-            test_loss += loss_fn(pred, y).item()
-            for pred_i, y_i in zip(pred, y):
-                correct[int(y_i)] += int(pred_i.argmax() == y_i)
-                class_size[int(y_i)] += 1
+        for batch, (embeddings, stance) in tqdm(enumerate(dataloader)):
+            pred = model(*embeddings)
+            test_loss += loss_fn(pred, stance).item()
+            for pred_i, stance_i in zip(pred, stance):
+                correct[int(stance_i)] += int(pred_i.argmax() == stance_i)
+                class_size[int(stance_i)] += 1
 
-    test_loss /= num_batches
+    test_loss /= BATCH_SIZE
     print("Per-class avg:")
     print(f"{[correct[i] / class_size[i] for i in class_size.keys()]}")
     print(f"Avg loss: {test_loss:>8f}\n")
-    
+
     wandb.log({"val-loss": test_loss})
     wandb.log({
        f'class-{i}':correct[i] / class_size[i] for i in class_size.keys()
     })
     print("*"*50)
 
-dataset = FakeNewsDataset('./data/combined_stances_train.csv', './data/combined_bodies_train.csv', related_only=True)
-SUBSET_SIZE = len(dataset)
-
-# Partition dataset into train and val sets
-dataset_size = SUBSET_SIZE
+##
+dataset = FakeNewsEncodedDataset(
+    stances_file="data/train_stances.csv.stance.dat",
+    bodies_file="data/train_bodies.csv.body.dat"
+)
+dataset_size = len(dataset)
 dataset_indices = list(range(dataset_size))
-cutoff = math.floor(dataset_size * 0.7)
+cutoff = math.floor(dataset_size * VAL_CUTOFF)
+
 train_sampler = SubsetRandomSampler(dataset_indices[:cutoff])
 val_sampler = SubsetRandomSampler(dataset_indices[cutoff:])
 
-train_dataloader = torch.utils.data.DataLoader(dataset, batch_size=BATCH_SIZE, sampler=train_sampler)
-val_dataloader = torch.utils.data.DataLoader(dataset, batch_size=BATCH_SIZE, sampler=val_sampler)
+train_dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, sampler=train_sampler)
+val_dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, sampler=val_sampler)
 
-model = AgreemNet()
-model.to(DEVICE)
-# wandb.watch(model, log_freq=1)
-
-for name, param in model.named_parameters():
-    if "encoder" in name:
-        param.requires_grad = False
-
-    print(name)
+model = AgreemFlat()
 
 loss_fn = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
-for epoch in range(EPOCHS):
-    train_loop(train_dataloader, model, loss_fn, optimizer)
+for ii in range(EPOCHS):
+    train(
+        dataloader=train_dataloader,
+        model=model,
+        loss_fn=loss_fn,
+        optimizer=optimizer,
+    )
